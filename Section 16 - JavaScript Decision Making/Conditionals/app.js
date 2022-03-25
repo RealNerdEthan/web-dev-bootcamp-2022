fbc96aaf7862308795112a0b883dc1d138fd81e0
Ethan Goddard <38088906+RealNerdEthan@users.noreply.github.com>
2022-03-25 18:35:19 -0400

    Comparison Operators and Conditionals
    We learned about comparison operators, equality, how to run JavaScript from a file, and started on conditional statements.

---
+++ Section 16 - JavaScript Decision Making/Conditionals/app.js
@@ -0,0 +1,8 @@
+let randomNumber = Math.floor(Math.random() * 2) + 1;
+
+if (randomNumber < 2) {
+    console.log("Your coin tolls result is heads!");
+} else {
+    console.log("Your coin tolls result is tails!")
+}
+
---
+++ Section 16 - JavaScript Decision Making/First JS Script/app.js
@@ -0,0 +1,4 @@
+let visitorNum = Math.floor(Math.random() * 10) + 1;
+
+console.log("Hello from my first JS file!");
+console.log(`You are visitor #${visitorNum}, congradulations! Boom! Boom!`);
