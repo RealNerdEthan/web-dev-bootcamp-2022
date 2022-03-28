b1bccadcb3bdaf3ab3c78d3556ecf5acb805cc2b
Ethan Goddard <38088906+RealNerdEthan@users.noreply.github.com>
2022-03-28 17:20:33 -0400

    Section 17 arrays
    We have started section 17 which is all about arrays. We learned how to create them, access values at specified indexes, and update values using indexOf.

---
+++ Section 17 - JavaScript Arrays/arrays.js
@@ -0,0 +1,11 @@
+//To make an empty array
+let students = [];
+
+//An array of strings
+let colors = ["red", "orange", "yellow"];
+
+//An array of numbers
+let lottoNumbers = [19, 22, 56, 7, 31];
+
+//A mixed array
+let stuff = [true, 68, "cat", null];
