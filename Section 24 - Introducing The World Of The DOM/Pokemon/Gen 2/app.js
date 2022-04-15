6f5fbb47688bc84bb8f8493df2a3d864dcc6aee2
Ethan Goddard <38088906+RealNerdEthan@users.noreply.github.com>
2022-04-14 21:18:10 -0400

    Updated Pokemon coding demo

--- Section 24 - Introducing The World Of The DOM/Pokemon/Gen 2/app.js
+++ Section 24 - Introducing The World Of The DOM/Pokemon/Gen 2/app.js
@@ -1,19 +1,17 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/
 
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-
-for (let i = 1; i <= 151; i++) {
+for (let i = 152; i <= 251; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
     const label = document.createElement('span');
     label.innerText = `#${i}`;
     const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
-
+    newImg.src = `${baseURL}${i}.png`;
 
     pokemon.appendChild(newImg);
     pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    container.appendChild(pokemon);
 }
